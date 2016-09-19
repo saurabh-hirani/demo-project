707e1dbbc8ebe888407169bf91ab80e8cf6bcefd
Saurabh Hirani <saurabh.hirani@gmail.com>
2016-09-19 11:00:35 +0530

    updated repo

--- package.sh
+++ package.sh
@@ -7,20 +7,23 @@ done
 
 output_dir=$1
 
+cd bin
+
 # script to package this repo via fpm
 fpm \
   -s dir \
   -t deb \
   -a "all" \
   -n "demo-project" \
-  -v "$(cat VERSION)" \
+  -v "$(cat ../VERSION)" \
   --maintainer "DemoCorp" \
   --provides "demo project" \
   --vendor "DemoCorp" \
   --before-install "hooks/before-install.sh" \
   --after-install "hooks/after-install.sh" \
   --before-upgrade "hooks/before-upgrade.sh" \
-  --after-upgrade "hooks/after-upgrade.sh"
+  --after-upgrade "hooks/after-upgrade.sh" \
+  *
 
 mkdir -p $output_dir
 mv *.deb $output_dir
